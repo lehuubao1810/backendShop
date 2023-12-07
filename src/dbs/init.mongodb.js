8c8bed7e493cec4b98143da95b602acd3dcbcd05
lehuubao1810 <lehuubao2909@gmail.com>
2023-12-07 11:19:50 +0700

    Update MONGODB_URI in init.mongodb.js

--- src/dbs/init.mongodb.js
+++ src/dbs/init.mongodb.js
@@ -3,7 +3,7 @@ import dotenv from "dotenv"
  
 dotenv.config();
 
-const MONGODB_URI = process.env.MONGODB_URI;
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb+srv://shopbb:gJhMeEiB0DLp9gAa@cluster0.9gyl6zf.mongodb.net/shopbb';
 
 const connectMongoDB = async () => {
   try {
